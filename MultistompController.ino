199228f069108bbbe6e1cb17b2c8c457434245c1
Hamilton Gerlach <andyx.d2@gmail.com>
2020-08-26 21:09:28 +0200

    Code cleanup

--- MultistompController.ino
+++ MultistompController.ino
@@ -8,17 +8,20 @@
 #define MIDI_SERIAL_RX 12
 #define MIDI_SERIAL_TX 11
 
-#define MIDI_IN_TIMEOUT 99999999
 #define MIDI_BAUDRATE 31250
 #define DEBUG_BAUDRATE 9600
 
 #define SWITCH_DEB 1000
 #define SWITCH_TUNER 14
 
-#define BUFFER_LENGTH 16
-
 #define CONTROL_BYTE 0xFF
 #define CONTROL_FLUSH true
+
+#define MIDI_STREAM MIDI_S
+#define MIDI_CHANNEL 2
+
+#define USB_STREAM Serial
+#define USB_CHANNEL 1
 
 
 // CLASSES
@@ -87,14 +90,13 @@ unsigned long TimerStart, TimerCurrent;
 
 byte PN1, PN2, CN1, CV1, CN2, CV2;
 bool RxNormCC1, RxNormCC2, RxTermCC1, RxTermCC2;
-bool RxBankCC1, RxBankCC2, RxPC1, RxPC2, RxActive;
+bool RxPC1, RxPC2, RxActive;
 
 AltSoftSerial MIDI_S = AltSoftSerial(MIDI_SERIAL_RX, MIDI_SERIAL_TX);
 
 
 // MAIN INIT FUNCTION
 void setup() {
-  MIDI_S.setTimeout(MIDI_IN_TIMEOUT);   // MIDI In Timeout
   MIDI_S.begin(MIDI_BAUDRATE);          // MIDI In/Out
   MIDI_S.flush(); Serial.flush();
   
@@ -113,38 +115,34 @@ void setup() {
 
 // MAIN LOOP FUNCTION
 void loop() {
-  while ((MIDI_S.available() > 0) && RxActive) {
-    Mem3.Push(MIDI_S.read());
+  while ((MIDI_STREAM.available() > 0) && RxActive) {
+    Mem3.Push(MIDI_STREAM.read());
     
-    if (Mem3.CCBank(1))   RxBankCC1 = true;
     if (Mem3.CCTerm(1))   RxTermCC1 = true;
     if (Mem3.CCNorm(1)) { RxNormCC1 = true; CN1 = Mem3.Data[1]; CV1 = Mem3.Data[2]; }
-      
-    if (Mem3.CCBank(2))   RxBankCC2 = true;
+    
     if (Mem3.CCTerm(2))   RxTermCC2 = true;
     if (Mem3.CCNorm(2)) { RxNormCC2 = true; CN2 = Mem3.Data[1]; CV2 = Mem3.Data[2]; }
 
     if (!RxPC1) {
       PN1 = Mem3.PC(1);
-      if (PN1 < CONTROL_BYTE) RxPC1 = true;
-    }
-
+      if (PN1 < CONTROL_BYTE) RxPC1 = true; }
+    
     if (!RxPC2) {
       PN2 = Mem3.PC(2);
-      if (PN2 < CONTROL_BYTE) RxPC2 = true;
-    }
+      if (PN2 < CONTROL_BYTE) RxPC2 = true; }
   }
   
 
   // STATE CHECK & SENDING ROUTINE
   if (StateMachine()) {
-    MidiOutCC2(0x4A, 0x00);   // Tuner off message
+    MidiOutCC(&USB_STREAM, 1, 0x4A, 0x00);   // Tuner off message
 
-    if (RxNormCC1) OnReceiveCC1(CN1, CV1);
-    if (RxPC1)     OnReceivePC1(PN1);
+    if (RxNormCC1) OnReceiveCC(&USB_STREAM, USB_CHANNEL, CN1, CV1);
+    if (RxPC1)     OnReceivePC(&USB_STREAM, USB_CHANNEL, PN1);
     
-    if (RxNormCC2) OnReceiveCC2(CN2, CV2);
-    if (RxPC2)     OnReceivePC2(PN2);
+    if (RxNormCC2) OnReceiveCC(&MIDI_STREAM, MIDI_CHANNEL, CN2, CV2);
+    if (RxPC2)     OnReceivePC(&MIDI_STREAM, MIDI_CHANNEL, PN2);
 
     ResetState();
     // TODO: start RxActive timer
@@ -154,11 +152,7 @@ void loop() {
   // TUNER SWITCH HANDLING
   TimerCurrent = millis();
   if ((digitalRead(SWITCH_TUNER) != HIGH) && (TimerCurrent - TimerStart > SWITCH_DEB)) {
-    //MidiOutCC2(0x4A, 0x7F);   // Tuner on message
-
-    ZoomParamEnable();
-    ZoomSwitchOn(1);
-    ZoomParamDisable();
+    MidiOutCC(&USB_STREAM, USB_CHANNEL, 0x4A, 0x7F);   // Tuner on message
     
     TimerStart = TimerCurrent;
   }
@@ -178,15 +172,8 @@ void ResetState() {
   CN1 = 0; CN2 = 0;
   CV1 = 0; CV2 = 0;
   
-  RxBankCC1 = false;
-  RxNormCC1 = false;
-  RxTermCC1 = false;
-  RxPC1     = false;
-  
-  RxBankCC2 = false;
-  RxNormCC2 = false;
-  RxTermCC2 = false;
-  RxPC2     = false;
+  RxNormCC1 = false; RxTermCC1 = false; RxPC1 = false;
+  RxNormCC2 = false; RxTermCC2 = false; RxPC2 = false;
   RxActive  = true;
   
   Mem3.Flush();
@@ -194,74 +181,55 @@ void ResetState() {
 
 
 // SEND EVENTS
-void OnReceivePC1(byte PN) {
+void OnReceivePC(Stream *Com, byte Channel, byte PN) {
   #if DEBUG
     Serial.println("");
-    Serial.print("TX: Channel 1 PC ");
+    Serial.print("RX: Channel "); Serial.print(Channel, DEC); Serial.print(" PC ");
     Serial.println(PN, HEX);
   #endif
 
-  if (PN < 50) MidiOutPC1(PN);
+  if (PN < 50) MidiOutPC(Com, Channel, PN);
   else {
-    
-  }
-}
-void OnReceivePC2(byte PN) {
-  #if DEBUG
-    Serial.println("");
-    Serial.print("TX: Channel 2 PC ");
-    Serial.println(PN, HEX);
-  #endif
-  
-  if (PN < 50) MidiOutPC2(PN);
-  else {
-    
+    MidiOutPC(Com, Channel, PN); // TODO
   }
 }
 
-void OnReceiveCC1(byte CN, byte CV) {
+void OnReceiveCC(Stream *Com, byte Channel, byte CN, byte CV) {
   #if DEBUG
     Serial.println("");
-    Serial.print("TX: Channel 1 CC ");
+    Serial.print("RX: Channel "); Serial.print(Channel, DEC); Serial.print(" CC ");
     Serial.print(CN, HEX);
     Serial.print(" ");
     Serial.println(CV, HEX);
   #endif
   
-  MidiOutCC1(CN, CV);
+  MidiOutCC(Com, Channel, CN, CV);
 }
-void OnReceiveCC2(byte CN, byte CV) {
-  #if DEBUG
-    Serial.println("");
-    Serial.print("TX: Channel 2 CC ");
-    Serial.print(CN, HEX);
-    Serial.print(" ");
-    Serial.println(CV, HEX);
-  #endif
-  
-  MidiOutCC2(CN, CV);
-}
-
 
 // MIDI OUT FUNCTIONS
-void MidiOutPC1(byte pn) {
-  MIDI_S.write(0xC0);
-  MIDI_S.write(pn);
-}
-void MidiOutPC2(byte pn) {
-  Serial.write(0xC0);
-  Serial.write(pn);
+void MidiOutPC(Stream *Com, byte Channel, byte PN) {
+  #if DEBUG
+    Serial.println("");
+    Serial.print("TX: Channel "); Serial.print(Channel, DEC); Serial.print(" PC ");
+    Serial.println(PN, HEX);
+  #endif
+  
+  Com->write(0xC0 + Channel - 1);
+  Com->write(PN);
 }
 
-void MidiOutCC1(byte cn, byte cv) {
-  MIDI_S.write(0xB0);
-  MIDI_S.write(cn);
-  MIDI_S.write(cv);
-}
-void MidiOutCC2(byte cn, byte cv) {
-  Serial.write(0xB0);
-  Serial.write(cn);
-  Serial.write(cv);
+void MidiOutCC(Stream *Com, byte Channel, byte CN, byte CV) {
+  #if DEBUG
+    Serial.println("");
+    Serial.print("TX: Channel "); Serial.print(Channel, DEC); Serial.print(" CC ");
+    Serial.print(CN, HEX);
+    Serial.print(" ");
+    Serial.println(CV, HEX);
+  #endif
+  
+  Com->write(0xB0 + Channel - 1);
+  Com->write(CN);
+  Com->write(CV);
 }
 
 void ZoomParamEnable() {
